0259cbe9cd9624989020a6f313cd925e456ee9a8
M Wayl <mwayluddin@gmail.com>
2024-02-12 16:47:21 +0500

    updated

--- src/components/nav-bar.jsx
+++ src/components/nav-bar.jsx
@@ -1,13 +1,7 @@
 import React, { useState } from "react";
-// import IconButton from '@mui/material/IconButton';
-// import CloseIcon from '@mui/icons-material/Close';
-// import { MenuOutlined } from "@material-ui/icons";
 import {  HashLink as Link} from "react-router-hash-link";
 import './style/nav-bar.scss';
-import Main from './main.jsx';
-import About from './about.jsx';
-import Portfolio from './portfolio.jsx';
-import Footer from './footer.jsx';
+
 
 function NavBar() {
   const [active, setActive] = useState(false);
@@ -23,6 +17,9 @@ function NavBar() {
 
         <ul className={active ? 'navbar_active' : 'navbar'}>
 
+        <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16" className="close" onClick={showMenu}>
+  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+</svg>
           {/* <IconButton> */}
             {/* <CloseIcon className="close" onClick={showMenu} /> */}
           {/* </IconButton> */}
@@ -35,6 +32,10 @@ function NavBar() {
        
 
         <div className="changer">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16" className="menu" onClick={showMenu}>
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+</svg>
+
           {/* <MenuOutlined className="menu" onClick={showMenu} /> */}
         </div>
       </nav>
